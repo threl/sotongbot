296f02f70393312185f8b0d2500d7108859a0620
threl <threl.d.9@gmail.com>
2019-07-06 00:07:16 +0700

    Add host command

--- bot.py
+++ bot.py
@@ -26,10 +26,15 @@ def get_valid_game_type(gametype = ""):
     }.get(gametype, "league")
 
 @bot.command()
+async def host(ctx):
+    await ctx.message.channel.send(file=discord.File('host.png'))
+
+@bot.command()
 async def info(ctx):
-    embed = discord.Embed(title="Leek", description="Daftar commands:", color=0xeee657)
+    embed = discord.Embed(title="SotongBot", description="Daftar commands:", color=0xeee657)
 
     embed.add_field(name="!kuy [maen]", value="[maen] bisa diganti dengan league/salmon/next/pb/campursari", inline=False)
+    embed.add_field(name="!host", value="menampilkan profile host mabar kebanggaan kita bersama", inline=False)
     await ctx.send(embed=embed)
 
 bot.run(os.environ['BOT_TOKEN'])
